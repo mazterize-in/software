d484e3da9acf8969670d9c08b0b9d85771fd52de
mazterize-in <newusernotfound643@gmail.com>
2025-05-20 09:52:42 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'Arial', padding: '2rem', textAlign: 'center' }}>
+      <h1>Welcome to MAZTERIZE.IN</h1>
+      <img
+        src="/profile.jpg"
+        alt="MAZTERIZE Logo"
+        width={200}
+        height={200}
+        style={{ borderRadius: '100px', margin: '1rem 0' }}
+      />
+      <p>
+        MAZTERIZE Provides Cracks, Serial Keys, Patches, Activators, Keygens, For Any PC Software Without Surveys.
+      </p>
+    </div>
+  );
+}
